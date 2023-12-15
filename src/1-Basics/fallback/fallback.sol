89c45009198ea21afd3a372626a90f65e86ef3b2
Alok Kumar <rahul05alok@gmail.com>
2023-12-15 22:52:52 +0530

    All about fallback

---
+++ src/1-Basics/fallback/fallback.sol
@@ -0,0 +1,55 @@
+// Layout of Contract:
+// version
+// imports
+// errors
+// interfaces, libraries, contracts
+// Type declarations
+// State variables
+// Events
+// Modifiers
+// Functions
+
+// Layout of Functions:
+// constructor
+// receive function (if exists)
+// fallback function (if exists)
+// external
+// public
+// internal
+// private
+// view & pure functions
+
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.17;
+
+contract Fallback {
+    event Log(string func, uint gas);
+
+    // Fallback function must be declared as external.
+    fallback() external payable {
+        // send / transfer (forwards 2300 gas to this fallback function)
+        // call (forwards all of the gas)
+        emit Log("fallback", gasleft());
+    }
+
+    // Receive is a variant of fallback that is triggered when msg.data is empty
+    receive() external payable {
+        emit Log("receive", gasleft());
+    }
+
+    // Helper function to check the balance fo this contract
+    function getBalance() public view returns(uint) {
+        return address(this).balance;
+    }
+}
+
+contract SendToFallback {
+    function transferToFallback(address payable _to) public payable {
+        _to.transfer(msg.value);
+    }
+
+    function callFallback(address payable _to) public payable {
+        (bool sent,) = _to.call{value:msg.value}("");
+        require(sent, "Failed to send Ether");
+    }
+}
---
+++ src/1-Basics/fallback/fallbackInputOutput.sol
@@ -0,0 +1,64 @@
+// Layout of Contract:
+// version
+// imports
+// errors
+// interfaces, libraries, contracts
+// Type declarations
+// State variables
+// Events
+// Modifiers
+// Functions
+
+// Layout of Functions:
+// constructor
+// receive function (if exists)
+// fallback function (if exists)
+// external
+// public
+// internal
+// private
+// view & pure functions
+ 
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.17;
+
+// TestFallbackInputOutput -> FallbackInputOutput -> Counter
+contract FallbackInputOutput {
+    address immutable target;
+
+    constructor(address _target) {
+        target = _target;
+    }
+
+    fallback(bytes calldata data) external payable returns (bytes memory) {
+        (bool ok, bytes memory res) = target.call{value:msg.value}(data);
+        require(ok, "call failed");
+        return res;
+    }
+}
+
+
+contract Counter {
+    uint public count;
+
+    function get() external view returns (uint) {
+        return count;
+    }
+
+    function inc() external returns (uint) {
+        count += 1;
+        return count;
+    }
+}
+
+contract TestFallbackInputOutput {
+    event Log(bytes res);
+
+    function test(address _fallback, bytes calldata data) external {
+        (bool ok, bytes memory res) = _fallback.call(data);
+    }
+
+    function getTestData() external pure returns (bytes memory, bytes memory) {
+        return (abi.encodeCall(Counter.get, ()), abi.encodeCall(Counter.inc, ()));
+    }
+}
