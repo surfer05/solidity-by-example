f462ace82ae0bde1236e4a310e74dee2ff994b35
Alok Kumar <rahul05alok@gmail.com>
2023-12-15 23:32:05 +0530

    DelegatCall

---
+++ src/1-Basics/delegateCall.sol
@@ -0,0 +1,50 @@
+// Layout of Contract:
+// version
+// imports
+// errors
+// interfaces, libraries, contracts
+// Type declarations
+// State variables
+// Events
+// Modifiers
+// Functions
+
+// Layout of Functions:
+// constructor
+// receive function (if exists)
+// fallback function (if exists)
+// external
+// public
+// internal
+// private
+// view & pure functions
+
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.17;
+
+// Note : Deploy this contact first
+contract B {
+    // Note: storage layout must be the same as contract A
+    uint public num;
+    address public sender;
+    uint public value;
+
+    function setVars(uint _num) public payable {
+        num = _num;
+        sender = msg.sender;
+        value = msg.value;
+    }
+}
+
+contract A {
+    uint public num;
+    address public sender;
+    uint public value;
+
+    function setVars(address _contract, uint _num) public payable {
+        // A's storage is set, B is not modified.
+        (bool success, bytes memory data) = _contract.delegatecall(
+            abi.encodeWithSignature(("setVars(uint256)"), _num)
+        );
+    }
+}
