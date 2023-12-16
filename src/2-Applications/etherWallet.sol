396465a33907720c7213d2acc63698f8123396a7
Alok Kumar <rahul05alok@gmail.com>
2023-12-16 13:04:13 +0530

    A basic wallet

---
+++ src/2-Applications/etherWallet.sol
@@ -0,0 +1,42 @@
+// Layout of Contract:
+// version
+// imports
+// errors
+// interfaces, libraries, contracts
+// Type declarations
+// State variables
+// Events
+// Modifiers
+// Functions
+
+// Layout of Functions:
+// constructor
+// receive function (if exists)
+// fallback function (if exists)
+// external
+// public
+// internal
+// private
+// view & pure functions
+ 
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.17;
+
+contract EtherWallet {
+    address payable public owner;
+
+    constructor() {
+        owner = payable(msg.sender);
+    }
+
+    receive() external payable {}
+
+    function withdraw(uint _amount) external {
+        require(msg.sender == owner, "caller is not owner");
+        payable(msg.sender).transfer(_amount);
+    }
+
+    function getBalance() external view returns (uint) {
+        return address(this).balance;
+    }
+}
