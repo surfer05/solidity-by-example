9694989e5bc45da3df18ef856b3752be4adc4c2a
Alok Kumar <rahul05alok@gmail.com>
2023-12-17 16:34:29 +0530

    Foundry upgraded

---
+++ src/3-Hacks/selfdestruct.sol
@@ -0,0 +1,8 @@
+// selfdestruct sends all remaining Ether stored in the contract to a designated address
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+contract EtherGame {
+    uint public targetAmount;
+}
