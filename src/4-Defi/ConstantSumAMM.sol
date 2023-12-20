c05a9ff01dcda9cb7a8df919524ae7a5a3106147
Alok Kumar <rahul05alok@gmail.com>
2023-12-20 12:55:23 +0530

    Chainlink Price Oracle

---
+++ src/4-Defi/ConstantSumAMM.sol
@@ -0,0 +1,70 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.19;
+
+interface IERC20 {
+    function totalSupply() external view returns (uint);
+
+    function balanceOf(address account) external view returns (uint);
+
+    function transfer(address recipient, uint amount) external returns (bool);
+
+    function allowance(
+        address owner,
+        address spender
+    ) external view returns (uint);
+
+    function approve(address spender, uint amount) external returns (bool);
+
+    function transferFrom(
+        address sender,
+        address recipient,
+        uint amount
+    ) external returns (bool);
+
+    event Transfer(address indexed from, address indexed to, uint amount);
+    event Approval(address indexed owner, address indexed spender, uint amount);
+}
+
+contract CSAMM {
+    IERC20 public immutable token0;
+    IERC20 public immutable token1;
+
+    uint public reserve0;
+    uint public reserve1;
+
+    uint public totalSupply;
+    mapping(address => uint) public balanceOf;
+
+    constructor(address _token0, address _token1) {
+        // Note: Tihs contract assumes that token0 and token1 both have same decimals
+        token0 = IERC20(_token0);
+        token1 = IERC20(_token1);
+    }
+
+    function _mint(address _to, uint _amount) private {
+        balanceOf[_to] += _amount;
+        totalSupply += _amount;
+    }
+
+    function _burn(address _from, uint _amount) private {
+        balanceOf[_from] -= _amount;
+        totalSupply -= _amount;
+    }
+
+    function _update(uint _res0, uint _res1) private {
+        reserve0 = _res0;
+        reserve1 = _res1;
+    }
+
+    function swap(
+        address _tokenIn,
+        uint _amountIn
+    ) external returns (uint amountOut) {
+        require(
+            _tokenIn == address(token0) || _tokenIn == address(token1),
+            "invalid token"
+        );
+
+        bool isToken0 = _tokenIn == address(token0);
+    }
+}
